0e85a0c28d0ae06df6a1bed381c300882f2325fd
bapun <mrutyunjayagadanayak@yandex.com>
2025-01-04 21:34:25 +0530

    Menu quit updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 from ui.mainwindow import Ui_MainWindow
+from PySide6.QtCore import QCoreApplication
 
 
 class MainWindow(QMainWindow):
@@ -9,10 +10,16 @@ class MainWindow(QMainWindow):
         super(MainWindow,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.addButton.clicked.connect(self.on_button_clicked)
+        self.ui.addButton.clicked.connect(self.on_addbutton_clicked)
+        self.ui.removeButton.clicked.connect(self.on_removebutton_clicked)
+        self.ui.actionQuit.triggered.connect(QCoreApplication.quit)
 
-    def on_button_clicked(self):
-        print("button clicked")
+    def on_addbutton_clicked(self):
+        print("add button clicked")
+
+    def on_removebutton_clicked(self):
+        print("Remove button clicked")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

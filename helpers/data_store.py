31b66cde0084e8aa5d0786ca9a65fc5f88e0c1fb
bapun <mrutyunjayagadanayak@yandex.com>
2025-01-05 19:25:30 +0530

    added some new logic for data store

---
+++ helpers/data_store.py
@@ -0,0 +1,22 @@
+import feedparser
+
+class Data_Store:
+    def __init__(self):
+        self.feeds_data = {}
+
+    def add_to_feed(self,url):
+        new_feed = feedparser.parse(url)
+        title = new_feed['feed']['title']
+
+        items = []
+
+        for entry in new_feed.entries:
+            item = {
+                'title' : entry.title,
+                'details' : entry.summary
+            }
+            items.append(item)
+
+        self.feeds_data[title] = items
+        print(self.feeds_data[title])
+        return title
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import sys
 import feedparser
 from PySide6.QtWidgets import QApplication, QMainWindow , QMessageBox
 from PySide6.QtCore import QFile
+
+from helpers.data_store import Data_Store
 from ui.mainwindow import Ui_MainWindow
 from PySide6.QtCore import QCoreApplication
-from helpers import url_validator
+from helpers.url_validator import url_validator
+import helpers.data_store
 
 
 class MainWindow(QMainWindow):
@@ -20,12 +23,14 @@ class MainWindow(QMainWindow):
         self.ui.add_button.clicked.connect(self.on_addbutton_clicked)
         self.ui.remove_button.clicked.connect(self.on_removebutton_clicked)
 
+        self.data_store = Data_Store()
+
 
     def on_addbutton_clicked(self):
         url = self.ui.feed_line_edit.text()
         if url_validator(url):
-            feed = feedparser.parse(url)
-            print(feed['feed']['title'])
+            self.ui.title_list.addItem(self.data_store.add_to_feed(url))
+
         else:
             QMessageBox().warning(self,"Invalid FEED","Entered feed URL is incorrect, Please enter a valid URL")
 
